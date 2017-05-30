049772a477e2834ed8186fbb4658d4c615b705d1
Naslaty Ali Kari <naslaty.ali-kari@creatis.insa-lyon.fr>
2017-05-30 18:02:30 +0200

    add of another scenario in the class

--- src/main/java/fr/insalyon/creatis/vip/vip_tester/ScenarioUpdateExe.java
+++ src/main/java/fr/insalyon/creatis/vip/vip_tester/ScenarioUpdateExe.java
@@ -10,14 +10,14 @@ import static org.junit.Assert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class Scenario2 {
+public class ScenarioUpdateExe {
 	
 	private VipTesterHelper vth = new VipTesterHelper();
 	private DefaultApi client = vth.getDefaultApi();
-	private static Logger logger = LoggerFactory.getLogger(Scenario2.class);
+	private static Logger logger = LoggerFactory.getLogger(ScenarioUpdateExe.class);
 		
 	//tries to modify an execution by changing name 
-	public boolean scenario2(String key) throws Exception{			
+	public boolean scenario2( String key) throws Exception{			
 		//execution history
 		String exeId = client.listExecutions().iterator().next().getIdentifier();
 		logger.debug("1");
@@ -26,32 +26,58 @@ public class Scenario2 {
 		//check a particular execution
 		Execution result1 = client.getExecution(exeId);
 		logger.debug("2");
-		assertNotNull("the execution Id is null",result1);
+		assertNotNull("the execution is null",result1);
 		
 		//modification of name parameter of the execution
 		String newName = vth.randomSelection();
 		logger.debug("3");
-		Execution body = vth.modifExecution(newName, 0L, vth.getAdditionTestPipelineIdString());
+		Execution body = vth.modifExecution(newName, 0L);
 		client.updateExecution(exeId, body);
 		
 		//check execution modification
 		Execution result2 = client.getExecution(exeId);
-		assertNotNull("the execution Id is null",result2);
+		assertNotNull("the execution is null",result2);
 		
 		boolean cond = !(result1.getName().equals(result2.getName())) || 
 					   !(result1.getTimeout().equals(result2.getTimeout()));
 		
 		assertThat("Timeout and name not modified", cond, is(true));
-
+		return cond;
+	}
+	
+	//tries to modify an execution but put the same name and the same timeout so NO modification
+	public boolean scenario5(String key) throws Exception{			
+		//find an execution to modify it
+		String exeId = client.listExecutions().iterator().next().getIdentifier();
+		assertNotNull("the execution Id is null",exeId);
+		
+		//check a particular execution
+		Execution result1 = client.getExecution(exeId);
+		assertNotNull("the execution is null",result1);
+		
+		//modification of name parameter of the execution
+		Execution body = vth.modifExecution(result1.getName(), result1.getTimeout());
+		client.updateExecution(exeId, body);
+		
+		//check execution modification
+		Execution result2 = client.getExecution(exeId);
+		assertNotNull("the execution is null",result2);
+		
+		boolean cond = (result1.getName().equals(result2.getName())) &&
+					   (result1.getTimeout().equals(result2.getTimeout()));
 		return cond;
 	}
 	
 	public static void main(String[] args) throws Exception{
 		
+		ScenarioUpdateExe scenarioTest2 = new ScenarioUpdateExe();
 		String apikey = System.getProperty("apikey");
 		logger.debug("Launch scenario 2");
 		logger.info("waited result: true");
-		Scenario2 scenarioTest2 = new Scenario2();
+		logger.info("Scenario 2 result: {}",scenarioTest2.scenario2(apikey));
+		
+		logger.debug("Launch scenario 5");
+		logger.info("waited result: true");
 		logger.info("Scenario 2 result: {}",scenarioTest2.scenario2(apikey));
 	}
 }
