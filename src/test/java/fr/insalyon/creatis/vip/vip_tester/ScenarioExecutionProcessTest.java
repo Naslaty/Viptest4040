d5cd97fff58c895eab51a758c1750eb6b0348037
Naslaty Ali Kari <naslaty.ali-kari@creatis.insa-lyon.fr>
2017-06-06 11:56:38 +0200

    split in little methods

--- src/test/java/fr/insalyon/creatis/vip/vip_tester/ScenarioExecutionProcessTest.java
+++ src/test/java/fr/insalyon/creatis/vip/vip_tester/ScenarioExecutionProcessTest.java
@@ -34,15 +34,65 @@ public class ScenarioExecutionProcessTest {
 	private DefaultApi client = vth.getDefaultApi();
 	private static Logger logger = LoggerFactory.getLogger(ScenarioExecutionProcess.class);
 	
+	public boolean searchPipelineId() throws Exception{
+		String pipelineId = null;
+		Iterator<Pipeline> listPipelineResultIt = client.listPipelines(null).iterator();
+
+		boolean isFound = false;
+		while(!isFound && listPipelineResultIt.hasNext()){
+			Pipeline p = listPipelineResultIt.next();
+			pipelineId = p.getIdentifier();
+			if(pipelineId.equals("AdditionTest/0.9")){
+				isFound = true;
+			}			
+		}
+		return isFound;
+	}
+	
+	public void checkPipelineParameter(Pipeline pipeline){
+		List<PipelineParameter> pipelineParam = pipeline.getParameters();
+		assertThat("It must have 3 parameters", pipelineParam.size(), is(3));
+		
+		int cmptInt = 0;
+		for(PipelineParameter pp : pipeline.getParameters()){
+			if(!(pp.getName().equals("results-directory"))){
+				if(pp.getType().equals(ParameterType.STRING)){
+					cmptInt++;
+				}
+			}
+		}
+		assertThat("It must have 2 parameters of type string for the addition", cmptInt, is(2)) ;		
+		return;
+	}
+	
+	public boolean checkExecutionProcess(boolean isFinished, final String executionId) throws Exception{
+		// TEST ExecutorService 1
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+
+		CallableTimeout callTimeout = new CallableTimeout();		
+		Callable<Boolean> callable = new Callable<Boolean>(){
+			public Boolean call() throws ApiException{
+				StatusEnum result = client.getExecution(executionId).getStatus();
+				return result.equals(StatusEnum.FINISHED);
+			}
+		};
+		
+		ScheduledFuture<Boolean> timeout = executor.schedule(callTimeout, 10,TimeUnit.MINUTES);
+		while(!isFinished){
+			 ScheduledFuture<Boolean>expected = executor.schedule(callable, 20, TimeUnit.SECONDS);
+			 callTimeout.setExpected(expected);
+			 isFinished = expected.get();
+		}
+		timeout.cancel(true);
+		executor.shutdownNow();
+		return isFinished;
+	}
+	
 	public class CallableTimeout implements Callable<Boolean>{
 		private ScheduledFuture<Boolean> expected = null;
 		
 		public Boolean call() throws InterruptedException, ExecutionException{
-			logger.debug("timeout call-->");
-			logger.debug("expected: {}", expected);
-			boolean b = expected.cancel(true);
-			logger.debug("cancel success {}",b);
-			return true;
+			return expected.cancel(true);
 		}
 		
 		public void setExpected(ScheduledFuture<Boolean> expected) {
@@ -52,67 +102,20 @@ public class ScenarioExecutionProcessTest {
 	
 	//tries to launch an execution an waits the end of it
 	@Test
-	public void scenario1Test() throws Exception{	
-		
-		//pipelines list
-		Iterator<Pipeline> listPipelineResultIt = client.listPipelines(null).iterator();
-		boolean bool = true;
-		String pipelineId = null;
-		while(bool && listPipelineResultIt.hasNext()){
-			Pipeline p = listPipelineResultIt.next();
-			pipelineId = p.getIdentifier();
-			if(pipelineId.equals("AdditionTest/0.9")){
-				bool = false;
-			}			
-		}
-		assertThat("AdditionTest/0.9 is not present", pipelineId, is("AdditionTest/0.9"));
+	public void scenario1Test() throws Exception{					
+		assertThat("AdditionTest/0.9 is not present", searchPipelineId(), is(true));
 		
 		//check parameters for a specified pipeline
 		Pipeline pipelineResult = client.getPipeline(vth.getAdditionTestPipelineId());
-		List<PipelineParameter> pipelineParam = pipelineResult.getParameters();
-		assertThat("It must have 3 parameters", pipelineParam.size(), is(3));
-
-		int cmptInt = 0;
-		for(PipelineParameter pp : pipelineResult.getParameters()){
-			if(!(pp.getName().equals("results-directory"))){
-				if(pp.getType().equals(ParameterType.STRING)){
-					cmptInt++;
-				}
-			}
-			logger.debug("type of parameters: {}",pp.getType());
-		}
-		assertThat("It must have 2 parameters of type string for the addition", cmptInt, is(2)) ;
+		checkPipelineParameter(pipelineResult);
 		
 		//create and start an execution
 		Execution result = client.initAndStartExecution(vth.initExecution("testScenario1", 40, 41));
-		assertThat("The status must be \"running\"", result.getStatus(), is(StatusEnum.RUNNING));
-		
+		assertThat("The status must be \"running\"", result.getStatus(), is(StatusEnum.RUNNING));		
 		//keep the identifier
-		final String exeId = result.getIdentifier();
-		
-		// TEST ExecutorService 1
-		ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-
-		CallableTimeout callTimeout = new CallableTimeout();
-		ScheduledExecutorService executorT = Executors.newSingleThreadScheduledExecutor();
-		
-		Boolean isFinished = false;
-		ScheduledFuture<Boolean> timeout = executorT.schedule(callTimeout, 10,TimeUnit.MINUTES);
-		while(!isFinished){
-			 ScheduledFuture<Boolean>expected = executor.schedule(new Callable<Boolean>(){
-				 							public Boolean call() throws ApiException{
-				 								StatusEnum result = client.getExecution(exeId).getStatus();
-				 								logger.debug("new check	result: {}", result);
-				 								return result.equals(StatusEnum.FINISHED);
-				 							}}, 20, TimeUnit.SECONDS);
-			 callTimeout.setExpected(expected);
-			 isFinished = expected.get();
-		}
-		timeout.cancel(true);
-		executor.shutdownNow();
-	
-		assertThat("The status must be \"finished\" but it is", isFinished, is(true));
-		
-		//return isFinished.equals(true);		
+		final String executionId = result.getIdentifier();
+				
+		boolean isFinished = false;
+		assertThat("The status must be \"finished\" but it is", checkExecutionProcess(isFinished, executionId), is(true));	
 	}
 }
